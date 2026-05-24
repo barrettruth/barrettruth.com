fcde8f4b3acfd85d1ab3901096914a8b5b21ff63
Barrett Ruth <br.barrettruth@gmail.com>
2026-05-24 18:16:05 -0500

    fix(astro): remove unused collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,1 @@
-import { defineCollection, z } from "astro:content";
-
-const base = z.object({
-  title: z.string(),
-  description: z.string().optional(),
-  date: z.string().optional(),
-  useKatex: z.boolean().optional(),
-  useD3: z.boolean().optional(),
-  scripts: z.array(z.string()).optional(),
-  redirect: z.string().optional(),
-});
-
-export const collections = {
-  x: defineCollection({ type: "content", schema: base }),
-};
+export const collections = {};
